5c567ba2e8593ec460a241d34fcd458e36f2bc74
Gurmehak Kaur Uppal <156244537+gurmehakkaur@users.noreply.github.com>
2026-07-23 16:26:31 -0400

    Add AdminCheckoutController for scene navigation

---
+++ MalibuLuminaHotel/src/main/java/ca/senecacollege/malibuluminahotel/controller/AdminCheckoutController.java
@@ -0,0 +1,13 @@
+package ca.senecacollege.malibuluminahotel.controller;
+
+import ca.senecacollege.malibuluminahotel.app.SceneNavigator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AdminCheckoutController {
+
+    @FXML
+    private void handleBack(ActionEvent event) {
+        SceneNavigator.switchScene(event, "AdminDashboard.fxml");
+    }
+}
