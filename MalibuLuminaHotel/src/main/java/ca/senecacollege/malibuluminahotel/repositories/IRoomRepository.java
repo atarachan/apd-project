5266822678aa972b0d1a47e963e1ca0243ed5e9c
Gurmehak Kaur Uppal <156244537+gurmehakkaur@users.noreply.github.com>
2026-07-23 17:45:24 -0400

    Update IRoomRepository.java

--- MalibuLuminaHotel/src/main/java/ca/senecacollege/malibuluminahotel/repositories/IRoomRepository.java
+++ MalibuLuminaHotel/src/main/java/ca/senecacollege/malibuluminahotel/repositories/IRoomRepository.java
@@ -3,6 +3,9 @@ package ca.senecacollege.malibuluminahotel.repositories;
 import ca.senecacollege.malibuluminahotel.models.Room;
 import ca.senecacollege.malibuluminahotel.models.RoomType;
 import ca.senecacollege.malibuluminahotel.models.enums.RoomStatus;
+import ca.senecacollege.malibuluminahotel.models.enums.RoomTypeName;
+
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,4 +18,7 @@ public interface IRoomRepository extends IRepository<Room, Long> {
     List<Room> findByRoomType(RoomType roomType);
 
     List<Room> findByFloor(int floor);
+
+    // Returns the first room of the given type that has no overlapping reservation during the date range.
+    Optional<Room> findFirstAvailable(RoomTypeName roomTypeName, LocalDate checkIn, LocalDate checkOut);
 }
