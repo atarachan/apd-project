d1dbe724fef42eca0f8ea3fabe4f443dfb919bbd
Gurmehak Kaur Uppal <156244537+gurmehakkaur@users.noreply.github.com>
2026-07-23 16:09:39 -0400

    Update AddOnsController.java

--- MalibuLuminaHotel/src/main/java/ca/senecacollege/malibuluminahotel/controller/AddOnsController.java
+++ MalibuLuminaHotel/src/main/java/ca/senecacollege/malibuluminahotel/controller/AddOnsController.java
@@ -1,48 +1,49 @@
 package ca.senecacollege.malibuluminahotel.controller;
 
+import ca.senecacollege.malibuluminahotel.app.BookingSession;
 import ca.senecacollege.malibuluminahotel.app.SceneNavigator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.CheckBox;
 
 public class AddOnsController {
 
+    @FXML private CheckBox breakfastCheckBox;
+    @FXML private CheckBox wifiCheckBox;
+    @FXML private CheckBox parkingCheckBox;
+    @FXML private CheckBox spaCheckBox;
+
     @FXML
     private void handleBack(ActionEvent event) {
-
-        SceneNavigator.switchScene(
-                event,
-                "ReservationDetails.fxml"
-        );
+        SceneNavigator.switchScene(event, "ReservationDetails.fxml");
     }
 
     @FXML
     private void handleContinue(ActionEvent event) {
+        BookingSession session = BookingSession.getInstance();
+        session.setBreakfastSelected(breakfastCheckBox.isSelected());
+        session.setWifiSelected(wifiCheckBox.isSelected());
+        session.setParkingSelected(parkingCheckBox.isSelected());
+        session.setSpaSelected(spaCheckBox.isSelected());
 
-        SceneNavigator.switchScene(
-                event,
-                "guestCheckout.fxml"
-        );
+        SceneNavigator.switchScene(event, "GuestCheckout.fxml");
     }
 
     @FXML
     private void handleRules(ActionEvent event) {
-
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-
         alert.setTitle("Hotel Rules");
         alert.setHeaderText("Malibu Lumina Hotel Rules");
-
         alert.setContentText(
                 "• Check-in begins at 3:00 PM\n\n" +
-                        "• Check-out before 11:00 AM\n\n" +
-                        "• Single Room: Maximum 2 Guests\n\n" +
-                        "• Double Room: Maximum 4 Guests\n\n" +
-                        "• Penthouse: Maximum 2 Guests\n\n" +
-                        "• Smoking is prohibited inside rooms\n\n" +
-                        "• Guests are responsible for damages"
+                "• Check-out before 11:00 AM\n\n" +
+                "• Single Room: Maximum 2 Guests\n\n" +
+                "• Double Room: Maximum 4 Guests\n\n" +
+                "• Penthouse: Maximum 2 Guests\n\n" +
+                "• Smoking is prohibited inside rooms\n\n" +
+                "• Guests are responsible for damages"
         );
-
         alert.showAndWait();
     }
 }
