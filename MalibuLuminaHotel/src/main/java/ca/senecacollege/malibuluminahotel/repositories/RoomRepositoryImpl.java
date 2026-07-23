cb5bdaf719af74dfccdc470a5db403027529e155
Gurmehak Kaur Uppal <156244537+gurmehakkaur@users.noreply.github.com>
2026-07-23 17:45:43 -0400

    Update RoomRepositoryImpl.java

--- MalibuLuminaHotel/src/main/java/ca/senecacollege/malibuluminahotel/repositories/RoomRepositoryImpl.java
+++ MalibuLuminaHotel/src/main/java/ca/senecacollege/malibuluminahotel/repositories/RoomRepositoryImpl.java
@@ -3,7 +3,10 @@ package ca.senecacollege.malibuluminahotel.repositories;
 import ca.senecacollege.malibuluminahotel.models.Room;
 import ca.senecacollege.malibuluminahotel.models.RoomType;
 import ca.senecacollege.malibuluminahotel.models.enums.RoomStatus;
+import ca.senecacollege.malibuluminahotel.models.enums.RoomTypeName;
+
 import javax.persistence.EntityManager;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -101,6 +104,33 @@ public class RoomRepositoryImpl extends AbstractRepository<Room, Long> implement
 
         }
         finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public Optional<Room> findFirstAvailable(RoomTypeName roomTypeName, LocalDate checkIn, LocalDate checkOut) {
+        EntityManager em = createEntityManager();
+
+        try {
+            return em.createQuery(
+                            "SELECT r FROM Room r " +
+                            "WHERE r.roomType.roomTypeName = :roomTypeName " +
+                            "AND r.roomId NOT IN (" +
+                            "  SELECT ri.room.roomId FROM ReservationItem ri " +
+                            "  WHERE ri.reservation.checkInDate < :checkOut " +
+                            "  AND ri.reservation.checkOutDate > :checkIn" +
+                            ") " +
+                            "ORDER BY r.roomNumber",
+                            Room.class)
+                    .setParameter("roomTypeName", roomTypeName)
+                    .setParameter("checkIn", checkIn)
+                    .setParameter("checkOut", checkOut)
+                    .setMaxResults(1)
+                    .getResultStream()
+                    .findFirst();
+
+        } finally {
             em.close();
         }
     }
